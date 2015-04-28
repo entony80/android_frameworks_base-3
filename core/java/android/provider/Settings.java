5fa10d32b0202b89674767b1b1325b2d8708ac1b
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-04-28 20:42:00 +0100

    Base: Add a controller for in-call proximity sensor (1/3)
    Change-Id: Icd17599ef3e0c45ee10fede7d663ea9a30446257
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3365,6 +3365,14 @@ public final class Settings {
         public static final Validator LOCKSCREEN_DISABLED_VALIDATOR = sBooleanValidator;
 
         /**
+
+        /**
+         * Whether to use the proximity sensor to turn the screen on/off during a call
+         * @hide
+         */
+        public static final String IN_CALL_PROXIMITY_SENSOR = "in_call_proximity_sensor";
+
+        /**
          * @deprecated Use {@link android.provider.Settings.Global#LOW_BATTERY_SOUND}
          * instead
          * @hide
