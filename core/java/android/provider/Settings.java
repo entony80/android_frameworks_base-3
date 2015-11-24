c3843b9ed941e6a70948eefd94416fb7f6cb63f0
LorDClockaN <davor@losinj.com>
2015-11-24 20:09:21 +0100

    FWB: Headset icon switch (1/2)
    Updated my lollipop patch so the switch works
    instantly, not only on headset re-plugg!
    Added needed permission for getting AUDIO_SERVICE,
    so that switch updates icon and overrides intent only
    when headset is plugged in.
    
    Change-Id: I561cf99935f9c1ddd0264640dc928bb29d45bdc5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3984,6 +3984,13 @@ public final class Settings {
          */
         public static final String SCREENSHOT_DELAY = "screenshot_delay";
 
+
+        /**
+         * Switch for headset icon
+         * @hide
+         */
+        public static final String SHOW_HEADSET_ICON = "show_headset_icon";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
