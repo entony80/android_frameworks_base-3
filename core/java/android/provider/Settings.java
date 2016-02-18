9f9473bd8f8fee31602bf3f1717a46bb4a341cf9
nilac8991 <nilac8991@gmail.com>
2016-02-18 09:53:42 +0100

    Revert "Revert "Squashed On-The-Go mode (1/2)""
    This reverts commit a48c253f239dc1d7b24a0b7126ebe84158fd3f87.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3731,6 +3731,36 @@ public final class Settings {
         };
 
         /**
+         * If On-The-Go should be displayed at the power menu.
+         *
+         * @hide
+         */
+        public static final String POWER_MENU_ONTHEGO_ENABLED = "power_menu_onthego_enabled";
+        
+        /**
+         * The alpha value of the On-The-Go overlay.
+         *
+         * @hide
+         */
+        public static final String ON_THE_GO_ALPHA = "on_the_go_alpha";
+
+        /**
+         * The camera instance to use.
+         * 0 = Rear Camera
+         * 1 = Front Camera
+         *
+         * @hide
+         */
+        public static final String ON_THE_GO_CAMERA = "on_the_go_camera";
+
+        /**
+         * Whether the service should restart itself or not.
+         *
+         * @hide
+         */
+        public static final String ON_THE_GO_SERVICE_RESTART = "on_the_go_service_restart";
+
+        /**
          * These are all public system settings
          *
          * @hide
