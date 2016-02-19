085ebe1090e3a5526786ccd7fe1ed92b196de2ab
nilac8991 <nilac8991@gmail.com>
2016-02-19 14:32:15 +0100

    Fix merge
    Signed-off-by: nilac8991 <nilac8991@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -158,8 +158,6 @@ public class NavigationBarView extends LinearLayout {
     private boolean mLayoutTransitionsEnabled = true;
     private boolean mWakeAndUnlocking;
 
-    private GestureDetector mDoubleTapGesture;
-    private boolean mDoubleTapToSleep;
 
     private class NavTransitionListener implements TransitionListener {
         private boolean mBackTransitioning;
@@ -343,19 +341,10 @@ public class NavigationBarView extends LinearLayout {
         if (mDeadZone != null && event.getAction() == MotionEvent.ACTION_OUTSIDE) {
             mDeadZone.poke(event);
         }
-<<<<<<< HEAD
         if (mDoubleTapToSleep) {
             mDoubleTapGesture.onTouchEvent(event);
         }
-=======
-        if (mDimNavButtonsTouchAnywhere) {
-            onNavButtonTouched();
-        }
-        if (mDoubleTapToSleep) {
-            mDoubleTapGesture.onTouchEvent(event);
-        }
-
->>>>>>> 4fa18f3... Fix DT2S on navbar after SlimDim
+        
         return super.onTouchEvent(event);
     }
 
@@ -1049,28 +1038,8 @@ public class NavigationBarView extends LinearLayout {
         }
 
         @Override
-<<<<<<< HEAD
         protected void update() {
-=======
-        public void onChange(boolean selfChange) {
-            mShowDpadArrowKeys = CMSettings.System.getIntForUser(getContext().getContentResolver(),
-                    CMSettings.System.NAVIGATION_BAR_MENU_ARROW_KEYS, 0, UserHandle.USER_CURRENT) != 0;
-            // reset saved side button visibilities
-            for (int i = 0; i < mSideButtonVisibilities.length; i++) {
-                for (int j = 0; j < mSideButtonVisibilities[i].length; j++) {
-                    mSideButtonVisibilities[i][j] = -1;
-                }
-            }
-            setNavigationIconHints(mNavigationIconHints, true);
-
-            super.onChange(selfChange);
-            update();
-            onNavButtonTouched();
-            setNavigationIconHints(mNavigationIconHints, true);
-        }
-
-        public void update() {
->>>>>>> 4fa18f3... Fix DT2S on navbar after SlimDim
+        
             ContentResolver resolver = mContext.getContentResolver();
 
             mShowDpadArrowKeys = CMSettings.System.getIntForUser(getContext().getContentResolver(),
@@ -1093,7 +1062,9 @@ public class NavigationBarView extends LinearLayout {
             mDimNavButtonsTouchAnywhere = (Settings.System.getIntForUser(resolver,
                     Settings.System.DIM_NAV_BUTTONS_TOUCH_ANYWHERE, 0,
                     UserHandle.USER_CURRENT) == 1);
-<<<<<<< HEAD
+            mDoubleTapToSleep = (Settings.System.getIntForUser(resolver,
+                    Settings.System.DOUBLE_TAP_SLEEP_NAVBAR, 0,
+                    UserHandle.USER_CURRENT) == 1);
             // reset saved side button visibilities
             for (int i = 0; i < mSideButtonVisibilities.length; i++) {
                 for (int j = 0; j < mSideButtonVisibilities[i].length; j++) {
@@ -1101,15 +1072,6 @@ public class NavigationBarView extends LinearLayout {
                 }
             }
             setNavigationIconHints(mNavigationIconHints, true);
-
-            onNavButtonTouched();
-            mDoubleTapToSleep = Settings.System.getIntForUser(mContext.getContentResolver(),
-                    Settings.System.DOUBLE_TAP_SLEEP_NAVBAR, 0, UserHandle.USER_CURRENT) != 0;
-=======
-	    mDoubleTapToSleep = (Settings.System.getIntForUser(resolver,
-                    Settings.System.DOUBLE_TAP_SLEEP_NAVBAR, 0,
-                    UserHandle.USER_CURRENT) == 1);
->>>>>>> 4fa18f3... Fix DT2S on navbar after SlimDim
         }
     }
 
