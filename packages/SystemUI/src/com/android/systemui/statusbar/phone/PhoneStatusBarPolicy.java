040ca25b26f0cafa9c5049fe582750ed22ab8ca7
Calin <nilac8991@gmail.com>
2016-03-25 18:58:07 +0100

    Implement XOSP Universal dialog for Shutdown/Reboot and Dexopting Process
    Based on and inspired by the following commit from Evisceration: NamelessRom/android_frameworks_base@7dc28503
    
    Based also on the two commits done by @AdmireTheDistance for the Alliance AOSP: AllianceAOSP/android_frameworks_base@b538004  and  AllianceAOSP/android_frameworks_base@eb5295e
    
    Signed-off-by: Calin <nilac8991@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -85,6 +85,7 @@ public class PhoneStatusBarPolicy implements Callback {
     private static final String SLOT_ALARM_CLOCK = "alarm_clock";
     private static final String SLOT_MANAGED_PROFILE = "managed_profile";
     private static final String SLOT_HEADSET = "headset";
+    private static final String SLOT_SU = "su"; 
 
     private final Context mContext;
     private final StatusBarManager mService;
