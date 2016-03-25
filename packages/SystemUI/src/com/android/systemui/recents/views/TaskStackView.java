e481cf8052d1fafcc29d8599d7224b89a36b9afc
Calin <nilac8991@gmail.com>
2016-03-25 19:08:49 +0100

    Revert "Adjustments in RAM Bar"
    This reverts commit dd737968a56f783eb6a3b8906727362f88d5d106.

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
@@ -50,7 +50,6 @@ import com.android.systemui.recents.model.RecentsPackageMonitor;
 import com.android.systemui.recents.model.RecentsTaskLoader;
 import com.android.systemui.recents.model.Task;
 import com.android.systemui.recents.model.TaskStack;
-import com.android.systemui.recents.views.RecentsView;
 import com.android.systemui.statusbar.DismissView;
 
 import java.util.ArrayList;
@@ -79,8 +78,6 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
         public void onTaskResize(Task t);
     }
     RecentsConfiguration mConfig;
-
-    RecentsView mView;
 
     TaskStack mStack;
     TaskStackViewLayoutAlgorithm mLayoutAlgorithm;
@@ -361,9 +358,6 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
                     @Override
                     public void onClick(View v) {
                         mStack.removeAllTasks();
-                        if (mView != null) {
-                            mView.updateMemoryStatus();
-                        }
                     }
                 });
                 addView(mDismissAllButton, 0);
