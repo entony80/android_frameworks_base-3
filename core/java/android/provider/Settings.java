5310abbd4c89013f41a2bd77238aa5ea493f8307
Calin <nilac8991@gmail.com>
2016-05-16 19:55:47 +0200

    Revert "Move KILL_APP_LONGPRESS_TIMEOUT to Settings.Secure"
    This reverts commit 5bb4e787d9debc937aee470232439be2ede85d9a.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2413,6 +2413,12 @@ public final class Settings {
         public static final String SHOW_SU_INDICATOR = "show_su_indicator";
 
         /**
+         * Timeout of long-pressing the Back button
+         * @hide
+         */
+        public static final String KILL_APP_LONGPRESS_TIMEOUT = "kill_app_longpress_timeout";
+
+        /**
          * Scaling factor for fonts, float.
          */
         public static final String FONT_SCALE = "font_scale";
@@ -6169,12 +6175,6 @@ public final class Settings {
          * @hide
          */
         public static final String SCREENSAVER_DEFAULT_COMPONENT = "screensaver_default_component";
-
-        /**
-         * Timeout of long-pressing the Back button
-         * @hide
-         */
-        public static final String KILL_APP_LONGPRESS_TIMEOUT = "kill_app_longpress_timeout";
 
         /**
          * The default NFC payment component
